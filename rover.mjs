5c67e04d18cc2060c66abe9d0d9187e3a4dbb74b
metamiaip <metamia@gmail.com>
2021-10-20 23:05:45 +0100

    Initial project - mars rover kata

---
+++ rover.mjs
@@ -0,0 +1,15 @@
+export function rover() {
+    return "I am Mars Rover!!!"
+}
+
+export function move() {
+    return "Mars Rover is moving!!!"
+}
+
+export function turn() {
+    return "Mars Rover is turning right or left!!!"
+}
+
+export function showRoverLocation() {
+    return "Display rover location (x,y,face)!"
+}
---
+++ rover.test.mjs
@@ -0,0 +1,7 @@
+import {rover,move,turn,showRoverLocation} from './rover.mjs';
+
+console.log(rover());
+console.log(move());
+console.log(turn());
+console.log(showRoverLocation());
+
