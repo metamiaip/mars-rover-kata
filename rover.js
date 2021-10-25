76c82490796918ff8f0ad9115e96a17bbe88cb20
metamiaip <metamia@gmail.com>
2021-10-25 21:07:52 +0100

    add functionality move and test

--- rover.js
+++ rover.js
@@ -5,19 +5,42 @@ function rover(size, position, faceTo, cmd) {
     let roverPos = (position === undefined) ? [0, 0] : position;
     let roverFaceTo = (faceTo === undefined) ? "N" : faceTo;
     const roverCmd = (cmd === undefined) ? "" : [...cmd];
+    let tmpRoverPos = (position === undefined) ? [0, 0] : position;
     //showRoverLocation(marsPlateauSize,roverPos,roverFaceTo);
 
     for(let i = 0; i < roverCmd.length; i++) {
         if (roverCmd[i] === 'L' || roverCmd[i] === 'R') {
             roverFaceTo = turn(roverFaceTo,roverCmd[i]);
+        } else if (roverCmd[i] === "M") {
+            tmpRoverPos = move(roverPos,roverFaceTo,roverCmd[i]);
+            if (tmpRoverPos[0]<0 || 
+                tmpRoverPos[1]<0 ||
+                tmpRoverPos[0]>marsPlateauSize[0] ||
+                tmpRoverPos[1]>marsPlateauSize[1] ) 
+                    //throw new Error("Invalid move command, rover stopped!");
+                    return "Invalid command, rover stopped!";
+            
+            roverPos = tmpRoverPos;
+        } else {
+            return "Invalid command, rover stopped!"; 
         }
+
     }
 
     return `Mars Plateau Size is [${marsPlateauSize}].\nRover initial position is [${initPos}].\nRover face to ${initFace}.\nCommand sent: ${roverCmd}.\nFinal rover position:${roverPos[0]} ${roverPos[1]} ${roverFaceTo}.`
 }
 
-function move(cmd) {
-    return "Mars Rover is moving!!!"
+function move(pos,faceTo,cmd) {
+    if (faceTo === "N") {
+        return [pos[0],pos[1]+1];
+    } else if (faceTo === 'E') { 
+        return [pos[0]+1,pos[1]];
+    } else if (faceTo === 'S') { 
+        return [pos[0],pos[1]-1];
+    } else if (faceTo === 'W') { 
+        return [pos[0]-1,pos[1]];
+    }
+    //return "Mars Rover is moving!!!"
 }
 
 function turn(faceTo,cmd) {
--- rover.test.js
+++ rover.test.js
@@ -11,11 +11,11 @@ describe("Rover test 01- return received pass in parameter", () => {
     });
   
     test("return the pass-in mars plateau size, and initial position", () => {
-        expect(rover([5,5],[0,1],"E","LMMM")).toBe("Mars Plateau Size is [5,5].\nRover initial position is [0,1].\nRover face to E.\nCommand sent: L,M,M,M.\nFinal rover position:0 1 N.");
+        expect(rover([5,5],[0,1],"E","L")).toBe("Mars Plateau Size is [5,5].\nRover initial position is [0,1].\nRover face to E.\nCommand sent: L.\nFinal rover position:0 1 N.");
       });     
 });
 
-describe("Rover test 02 - check turn function received correct command", () => {
+describe("Rover test 02 - check turn function", () => {
   test("check turn left (L)", () => {
     expect(rover([5,5],[0,1],"E","L")).toBe("Mars Plateau Size is [5,5].\nRover initial position is [0,1].\nRover face to E.\nCommand sent: L.\nFinal rover position:0 1 N.");
   });
@@ -23,4 +23,34 @@ describe("Rover test 02 - check turn function received correct command", () => {
   test("check turn right (R)", () => {
       expect(rover([5,5],[0,1],"E","R")).toBe("Mars Plateau Size is [5,5].\nRover initial position is [0,1].\nRover face to E.\nCommand sent: R.\nFinal rover position:0 1 S.");
     });     
+});
+
+describe("Rover test 03 - check move function", () => {
+  test("[0,0], face N, M M M", () => {
+    expect(rover([5,5],[0,0],"N","MMM")).toBe("Mars Plateau Size is [5,5].\nRover initial position is [0,0].\nRover face to N.\nCommand sent: M,M,M.\nFinal rover position:0 3 N.");
+  });
+  
+  test("[0,0], face N, L M", () => {
+    expect(rover([5,5],[0,0],"N","LM")).toBe("Invalid command, rover stopped!");
+  });
+});
+
+describe("Rover test 04 - test combined command with L,R,M", () => {
+  test("[1,2], face:N, command:LMLMLMLMM", () => {
+    expect(rover([5,5],[1,2],"N","LMLMLMLMM")).toBe("Mars Plateau Size is [5,5].\nRover initial position is [1,2].\nRover face to N.\nCommand sent: L,M,L,M,L,M,L,M,M.\nFinal rover position:1 3 N.");
+  });
+  
+  test("[3,3], face:E, command:MMRMMRMRRM", () => {
+    expect(rover([5,5],[3,3],"E","MMRMMRMRRM")).toBe("Mars Plateau Size is [5,5].\nRover initial position is [3,3].\nRover face to E.\nCommand sent: M,M,R,M,M,R,M,R,R,M.\nFinal rover position:5 1 E.");
+  });
+});
+
+describe("Rover test 05 - test combined command with L,R,M and invalid command", () => {
+  test("[1,2], face:N, command:LMX", () => {
+    expect(rover([5,5],[1,2],"N","LMX")).toBe("Invalid command, rover stopped!");
+  });
+  
+  test("[3,3], face:E, command:MM1", () => {
+    expect(rover([5,5],[3,3],"E","MM1")).toBe("Invalid command, rover stopped!");
+  });
 });
