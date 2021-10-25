ace74f1d3b8260e85e4f1edaae3cb02f7b8fb438
metamiaip <metamia@gmail.com>
2021-10-25 16:06:37 +0100

    Pass rover command and return

--- rover.js
+++ rover.js
@@ -1,9 +1,12 @@
-function rover(size, position, faceTo) {
+function rover(size, position, faceTo, cmd) {
     const marsPlateauSize = size;
     const roverPos = (position === undefined) ? [0, 0] : position;
     const roverFaceTo = (faceTo === undefined) ? "N" : faceTo;
+    const roverCmd = (cmd === undefined) ? "" : [...cmd];
+    //showRoverLocation(marsPlateauSize,roverPos,roverFaceTo);
 
-    return `Mars Plateau Size is [${marsPlateauSize}].\nRover initial position is [${roverPos}].\nRover face to ${roverFaceTo}.`
+    
+    return `Mars Plateau Size is [${marsPlateauSize}].\nRover initial position is [${roverPos}].\nRover face to ${roverFaceTo}.\nCommand sent: ${roverCmd}`
 }
 
 function move() {
@@ -14,8 +17,8 @@ function turn() {
     return "Mars Rover is turning right or left!!!"
 }
 
-function showRoverLocation() {
-    return "Display rover location (x,y,face)!"
+function showRoverLocation(size,pos,face) {
+    return return `Mars Plateau Size is [${size}].\nRover initial position is [${pos}].\nRover face to ${face}.`
 }
 
 module.exports = {
--- rover.test.js
+++ rover.test.js
@@ -11,6 +11,6 @@ describe("Rover test", () => {
     });
   
     test("return the pass-in mars plateau size, and initial position", () => {
-        expect(rover([5,5],[0,1],"E")).toBe("Mars Plateau Size is [5,5].\nRover initial position is [0,1].\nRover face to E.");
+        expect(rover([5,5],[0,1],"E","LRMMM")).toBe("Mars Plateau Size is [5,5].\nRover initial position is [0,1].\nRover face to E.Command sent: L,R,M,M,M.");
       });
 });
